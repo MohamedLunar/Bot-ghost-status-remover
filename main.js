359fd6a21bb4010df2f9ca7a3c19eb6011bb86bb
MohamedLunar <dreamtxk578@gmail.com>
2024-09-26 17:36:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 
   client.user.setActivity('?help | Made By MohamedLunar', { type: 'WATCHING' })
-    .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-    .catch(console.error);
+    
 });
 
 client.login(token);
